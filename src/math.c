15325a2d2328f354e97fcb61b85380b0f3b61cdc
Fabio de Souza Villaca Medeiros <fabiosvm@outlook.com>
2021-10-28 00:41:08 -0300

    added random() in math library

--- src/math.c
+++ src/math.c
@@ -4,6 +4,8 @@
 //
 
 #include "math.h"
+#include <stdlib.h>
+#include <time.h>
 #include <math.h>
 #include <assert.h>
 #include "common.h"
@@ -18,6 +20,7 @@ static int floor_call(vm_t *vm, value_t *frame);
 static int ceil_call(vm_t *vm, value_t *frame);
 static int pow_call(vm_t *vm, value_t *frame);
 static int sqrt_call(vm_t *vm, value_t *frame);
+static int random_call(vm_t *vm, value_t *frame);
 
 static int abs_call(vm_t *vm, value_t *frame)
 {
@@ -80,6 +83,14 @@ static int sqrt_call(vm_t *vm, value_t *frame)
   return vm_push_number(vm, sqrt(val.as.number));
 }
 
+static int random_call(vm_t *vm, value_t *frame)
+{
+  (void) frame;
+  srand((unsigned) time(NULL));
+  double result = (double) rand() / RAND_MAX;
+  return vm_push_number(vm, result);
+}
+
 #ifdef WIN32
 void __declspec(dllexport) __stdcall load_math(vm_t *vm)
 #else
@@ -92,6 +103,7 @@ void load_math(vm_t *vm)
   char ceil[] = "ceil";
   char pow[] = "pow";
   char sqrt[] = "sqrt";
+  char random[] = "random";
   struct_t *ztruct = struct_new(string_from_chars(-1, "math"));
   assert(struct_put_if_absent(ztruct, sizeof(pi) - 1, pi));
   assert(struct_put_if_absent(ztruct, sizeof(abs) - 1, abs));
@@ -99,12 +111,14 @@ void load_math(vm_t *vm)
   assert(struct_put_if_absent(ztruct, sizeof(ceil) - 1, ceil));
   assert(struct_put_if_absent(ztruct, sizeof(pow) - 1, pow));
   assert(struct_put_if_absent(ztruct, sizeof(sqrt) - 1, sqrt));
+  assert(struct_put_if_absent(ztruct, sizeof(random) - 1, random));
   assert(vm_push_number(vm, M_PI) == STATUS_OK);
   assert(vm_push_native(vm, native_new(string_from_chars(-1, abs), 1, &abs_call)) == STATUS_OK);
   assert(vm_push_native(vm, native_new(string_from_chars(-1, floor), 1, &floor_call)) == STATUS_OK);
   assert(vm_push_native(vm, native_new(string_from_chars(-1, ceil), 1, &ceil_call)) == STATUS_OK);
   assert(vm_push_native(vm, native_new(string_from_chars(-1, pow), 2, &pow_call)) == STATUS_OK);
   assert(vm_push_native(vm, native_new(string_from_chars(-1, sqrt), 1, &sqrt_call)) == STATUS_OK);
+  assert(vm_push_native(vm, native_new(string_from_chars(-1, random), 0, &random_call)) == STATUS_OK);
   assert(vm_push_struct(vm, ztruct) == STATUS_OK);
   vm_instance(vm);
 }
