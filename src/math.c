ee37328134a0dd3ef165b60487fc903bd66b382a
Fabio de Souza Villaca Medeiros <fabiosvm@outlook.com>
2022-02-05 14:30:22 -0300

    added in math module: sin(), cos(), tan(), asin(), acos(), atan()

--- src/math.c
+++ src/math.c
@@ -15,6 +15,12 @@
 #endif
 
 static int abs_call(vm_t *vm, value_t *args);
+static int sin_call(vm_t *vm, value_t *args);
+static int cos_call(vm_t *vm, value_t *args);
+static int tan_call(vm_t *vm, value_t *args);
+static int asin_call(vm_t *vm, value_t *args);
+static int acos_call(vm_t *vm, value_t *args);
+static int atan_call(vm_t *vm, value_t *args);
 static int floor_call(vm_t *vm, value_t *args);
 static int ceil_call(vm_t *vm, value_t *args);
 static int pow_call(vm_t *vm, value_t *args);
@@ -30,6 +36,72 @@ static int abs_call(vm_t *vm, value_t *args)
     return STATUS_ERROR;
   }
   return vm_push_number(vm, fabs(val.as.number));
+}
+
+static int sin_call(vm_t *vm, value_t *args)
+{
+  value_t val = args[1];
+  if (!IS_NUMBER(val))
+  {
+    runtime_error("type error: expected number but got `%s`", type_name(val.type));
+    return STATUS_ERROR;
+  }
+  return vm_push_number(vm, sin(val.as.number));
+}
+
+static int cos_call(vm_t *vm, value_t *args)
+{
+  value_t val = args[1];
+  if (!IS_NUMBER(val))
+  {
+    runtime_error("type error: expected number but got `%s`", type_name(val.type));
+    return STATUS_ERROR;
+  }
+  return vm_push_number(vm, cos(val.as.number));
+}
+
+static int tan_call(vm_t *vm, value_t *args)
+{
+  value_t val = args[1];
+  if (!IS_NUMBER(val))
+  {
+    runtime_error("type error: expected number but got `%s`", type_name(val.type));
+    return STATUS_ERROR;
+  }
+  return vm_push_number(vm, tan(val.as.number));
+}
+
+static int asin_call(vm_t *vm, value_t *args)
+{
+  value_t val = args[1];
+  if (!IS_NUMBER(val))
+  {
+    runtime_error("type error: expected number but got `%s`", type_name(val.type));
+    return STATUS_ERROR;
+  }
+  return vm_push_number(vm, asin(val.as.number));
+}
+
+static int acos_call(vm_t *vm, value_t *args)
+{
+  value_t val = args[1];
+  if (!IS_NUMBER(val))
+  {
+    runtime_error("type error: expected number but got `%s`", type_name(val.type));
+    return STATUS_ERROR;
+  }
+  return vm_push_number(vm, acos(val.as.number));
+}
+
+static int atan_call(vm_t *vm, value_t *args)
+{
+  value_t val = args[1];
+  if (!IS_NUMBER(val))
+  {
+    runtime_error("type error: expected number but got `%s`", type_name(val.type));
+    return STATUS_ERROR;
+  }
+  return vm_push_number(vm, atan(val.as.number));
 }
 
 static int floor_call(vm_t *vm, value_t *args)
@@ -85,7 +157,6 @@ static int sqrt_call(vm_t *vm, value_t *args)
 static int random_call(vm_t *vm, value_t *args)
 {
   (void) args;
-  srand((unsigned) time(NULL));
   double result = (double) rand() / RAND_MAX;
   return vm_push_number(vm, result);
 }
@@ -96,6 +167,7 @@ int __declspec(dllexport) __stdcall load_math(vm_t *vm)
 int load_math(vm_t *vm)
 #endif
 {
+  srand((unsigned) time(NULL));
   if (vm_push_string_from_chars(vm, -1, "math") == STATUS_ERROR)
     return STATUS_ERROR;
   if (vm_push_string_from_chars(vm, -1, "Pi") == STATUS_ERROR)
@@ -105,6 +177,30 @@ int load_math(vm_t *vm)
   if (vm_push_string_from_chars(vm, -1, "abs") == STATUS_ERROR)
     return STATUS_ERROR;
   if (vm_push_new_native(vm, "abs", 1, &abs_call) == STATUS_ERROR)
+    return STATUS_ERROR;
+  if (vm_push_string_from_chars(vm, -1, "sin") == STATUS_ERROR)
+    return STATUS_ERROR;
+  if (vm_push_new_native(vm, "sin", 1, &sin_call) == STATUS_ERROR)
+    return STATUS_ERROR;
+  if (vm_push_string_from_chars(vm, -1, "cos") == STATUS_ERROR)
+    return STATUS_ERROR;
+  if (vm_push_new_native(vm, "cos", 1, &cos_call) == STATUS_ERROR)
+    return STATUS_ERROR;
+  if (vm_push_string_from_chars(vm, -1, "tan") == STATUS_ERROR)
+    return STATUS_ERROR;
+  if (vm_push_new_native(vm, "tan", 1, &tan_call) == STATUS_ERROR)
+    return STATUS_ERROR;
+  if (vm_push_string_from_chars(vm, -1, "asin") == STATUS_ERROR)
+    return STATUS_ERROR;
+  if (vm_push_new_native(vm, "asin", 1, &asin_call) == STATUS_ERROR)
+    return STATUS_ERROR;
+  if (vm_push_string_from_chars(vm, -1, "acos") == STATUS_ERROR)
+    return STATUS_ERROR;
+  if (vm_push_new_native(vm, "acos", 1, &acos_call) == STATUS_ERROR)
+    return STATUS_ERROR;
+  if (vm_push_string_from_chars(vm, -1, "atan") == STATUS_ERROR)
+    return STATUS_ERROR;
+  if (vm_push_new_native(vm, "atan", 1, &atan_call) == STATUS_ERROR)
     return STATUS_ERROR;
   if (vm_push_string_from_chars(vm, -1, "floor") == STATUS_ERROR)
     return STATUS_ERROR;
@@ -126,5 +222,5 @@ int load_math(vm_t *vm)
     return STATUS_ERROR;
   if (vm_push_new_native(vm, "random", 0, &random_call) == STATUS_ERROR)
     return STATUS_ERROR;
-  return vm_construct(vm, 7);
+  return vm_construct(vm, 13);
 }
