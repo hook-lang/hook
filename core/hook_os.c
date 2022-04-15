92c2fa5c8d1db2f08fb6f8b53b86635713058b3a
Fabio de Souza Villaca Medeiros <fabiosvm@outlook.com>
2022-04-15 00:49:58 -0300

    added in os: time()

--- core/hook_os.c
+++ core/hook_os.c
@@ -8,6 +8,7 @@
 #include <time.h>
 
 static int32_t clock_call(hk_vm_t *vm, hk_value_t *args);
+static int32_t time_call(hk_vm_t *vm, hk_value_t *args);
 static int32_t system_call(hk_vm_t *vm, hk_value_t *args);
 static int32_t getenv_call(hk_vm_t *vm, hk_value_t *args);
 
@@ -15,6 +16,12 @@ static int32_t clock_call(hk_vm_t *vm, hk_value_t *args)
 {
   (void) args;
   return hk_vm_push_float(vm, (double) clock() / CLOCKS_PER_SEC);
+}
+
+static int32_t time_call(hk_vm_t *vm, hk_value_t *args)
+{
+  (void) args;
+  return hk_vm_push_float(vm, (double) time(NULL));
 }
 
 static int32_t system_call(hk_vm_t *vm, hk_value_t *args)
@@ -49,6 +56,10 @@ int32_t load_os(hk_vm_t *vm)
     return HK_STATUS_ERROR;
   if (hk_vm_push_new_native(vm, "clock", 0, &clock_call) == HK_STATUS_ERROR)
     return HK_STATUS_ERROR;
+  if (hk_vm_push_string_from_chars(vm, -1, "time") == HK_STATUS_ERROR)
+    return HK_STATUS_ERROR;
+  if (hk_vm_push_new_native(vm, "time", 0, &time_call) == HK_STATUS_ERROR)
+    return HK_STATUS_ERROR;
   if (hk_vm_push_string_from_chars(vm, -1, "system") == HK_STATUS_ERROR)
     return HK_STATUS_ERROR;
   if (hk_vm_push_new_native(vm, "system", 1, &system_call) == HK_STATUS_ERROR)
@@ -57,5 +68,5 @@ int32_t load_os(hk_vm_t *vm)
     return HK_STATUS_ERROR;
   if (hk_vm_push_new_native(vm, "getenv", 1, &getenv_call) == HK_STATUS_ERROR)
     return HK_STATUS_ERROR;
-  return hk_vm_construct(vm, 4);
+  return hk_vm_construct(vm, 5);
 }
