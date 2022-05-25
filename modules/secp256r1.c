4b88ca583e071fd95d868f79dd83b53f632e14b0
Fabio de Souza Villaca Medeiros <fabiosvm@outlook.com>
2022-05-24 22:08:53 -0300

    improved module secp256r1

--- modules/secp256r1.c
+++ modules/secp256r1.c
@@ -4,23 +4,29 @@
 //
 
 #include "secp256r1.h"
+#define ECC_CURVE 32
 #include "ecc.h"
 
+#define PUBLIC_KEY_SIZE  (ECC_BYTES + 1)
+#define PRIVATE_KEY_SIZE ECC_BYTES
+#define SECRET_SIZE      ECC_BYTES
+#define HASH_SIZE        ECC_BYTES
+#define SIGNATURE_SIZE   (ECC_BYTES << 1)
+
 static int32_t new_key_pair_call(hk_vm_t *vm, hk_value_t *args);
+static int32_t shared_secret_call(hk_vm_t *vm, hk_value_t *args);
 static int32_t sign_hash_call(hk_vm_t *vm, hk_value_t *args);
 static int32_t verify_signature_call(hk_vm_t *vm, hk_value_t *args);
 
 static int32_t new_key_pair_call(hk_vm_t *vm, hk_value_t *args)
 {
   (void) args;
-  int32_t pub_key_len = ECC_BYTES + 1;
-  hk_string_t *pub_key = hk_string_new_with_capacity(pub_key_len);
-  pub_key->length = pub_key_len;
-  pub_key->chars[pub_key_len] = '\0';
-  int32_t priv_key_len = ECC_BYTES;
-  hk_string_t *priv_key = hk_string_new_with_capacity(priv_key_len);
-  priv_key->length = priv_key_len;
-  priv_key->chars[priv_key_len] = '\0';
+  hk_string_t *pub_key = hk_string_new_with_capacity(PUBLIC_KEY_SIZE);
+  pub_key->length = PUBLIC_KEY_SIZE;
+  pub_key->chars[PUBLIC_KEY_SIZE] = '\0';
+  hk_string_t *priv_key = hk_string_new_with_capacity(PRIVATE_KEY_SIZE);
+  priv_key->length = PRIVATE_KEY_SIZE;
+  priv_key->chars[PRIVATE_KEY_SIZE] = '\0';
   (void) ecc_make_key((uint8_t *) pub_key->chars, (uint8_t *) priv_key->chars);
   hk_array_t *arr = hk_array_new_with_capacity(2);
   arr->length = 2;
@@ -36,6 +42,27 @@ static int32_t new_key_pair_call(hk_vm_t *vm, hk_value_t *args)
   return HK_STATUS_OK;
 }
 
+static int32_t shared_secret_call(hk_vm_t *vm, hk_value_t *args)
+{
+  if (hk_vm_check_string(args, 1) == HK_STATUS_ERROR)
+    return HK_STATUS_ERROR;
+  if (hk_vm_check_string(args, 2) == HK_STATUS_ERROR)
+    return HK_STATUS_ERROR;
+  hk_string_t *pub_key = hk_as_string(args[1]);
+  hk_string_t *priv_key = hk_as_string(args[2]);
+  hk_string_t *secret = hk_string_new_with_capacity(SECRET_SIZE);
+  secret->length = SECRET_SIZE;
+  secret->chars[SECRET_SIZE] = '\0';
+  (void) ecdh_shared_secret((uint8_t *) pub_key->chars, (uint8_t *) priv_key->chars,
+    (uint8_t *) secret->chars);
+  if (hk_vm_push_string(vm, secret) == HK_STATUS_ERROR)
+  {
+    hk_string_free(secret);
+    return HK_STATUS_ERROR;
+  }
+  return HK_STATUS_OK;
+}
+
 static int32_t sign_hash_call(hk_vm_t *vm, hk_value_t *args)
 {
   if (hk_vm_check_string(args, 1) == HK_STATUS_ERROR)
@@ -44,10 +71,9 @@ static int32_t sign_hash_call(hk_vm_t *vm, hk_value_t *args)
     return HK_STATUS_ERROR;
   hk_string_t *priv_key = hk_as_string(args[1]);
   hk_string_t *hash = hk_as_string(args[2]);
-  int32_t length = ECC_BYTES << 1;
-  hk_string_t *signature = hk_string_new_with_capacity(length);
-  signature->length = length;
-  signature->chars[length] = '\0';
+  hk_string_t *signature = hk_string_new_with_capacity(SIGNATURE_SIZE);
+  signature->length = SIGNATURE_SIZE;
+  signature->chars[SIGNATURE_SIZE] = '\0';
   (void) ecdsa_sign((uint8_t *) priv_key->chars, (uint8_t *) hash->chars,
     (uint8_t *) signature->chars);
   if (hk_vm_push_string(vm, signature) == HK_STATUS_ERROR)
@@ -82,9 +108,33 @@ int32_t load_secp256r1(hk_vm_t *vm)
 {
   if (hk_vm_push_string_from_chars(vm, -1, "secp256r1") == HK_STATUS_ERROR)
     return HK_STATUS_ERROR;
+  if (hk_vm_push_string_from_chars(vm, -1, "PUBLIC_KEY_SIZE") == HK_STATUS_ERROR)
+    return HK_STATUS_ERROR;
+  if (hk_vm_push_float(vm, PUBLIC_KEY_SIZE) == HK_STATUS_ERROR)
+    return HK_STATUS_ERROR;
+  if (hk_vm_push_string_from_chars(vm, -1, "PRIVATE_KEY_SIZE") == HK_STATUS_ERROR)
+    return HK_STATUS_ERROR;
+  if (hk_vm_push_float(vm, PRIVATE_KEY_SIZE) == HK_STATUS_ERROR)
+    return HK_STATUS_ERROR;
+  if (hk_vm_push_string_from_chars(vm, -1, "SECRET_SIZE") == HK_STATUS_ERROR)
+    return HK_STATUS_ERROR;
+  if (hk_vm_push_float(vm, SECRET_SIZE) == HK_STATUS_ERROR)
+    return HK_STATUS_ERROR;
+  if (hk_vm_push_string_from_chars(vm, -1, "HASH_SIZE") == HK_STATUS_ERROR)
+    return HK_STATUS_ERROR;
+  if (hk_vm_push_float(vm, HASH_SIZE) == HK_STATUS_ERROR)
+    return HK_STATUS_ERROR;
+  if (hk_vm_push_string_from_chars(vm, -1, "SIGNATURE_SIZE") == HK_STATUS_ERROR)
+    return HK_STATUS_ERROR;
+  if (hk_vm_push_float(vm, SIGNATURE_SIZE) == HK_STATUS_ERROR)
+    return HK_STATUS_ERROR;
   if (hk_vm_push_string_from_chars(vm, -1, "new_key_pair") == HK_STATUS_ERROR)
     return HK_STATUS_ERROR;
   if (hk_vm_push_new_native(vm, "new_key_pair", 0, &new_key_pair_call) == HK_STATUS_ERROR)
+    return HK_STATUS_ERROR;
+  if (hk_vm_push_string_from_chars(vm, -1, "shared_secret") == HK_STATUS_ERROR)
+    return HK_STATUS_ERROR;
+  if (hk_vm_push_new_native(vm, "shared_secret", 2, &shared_secret_call) == HK_STATUS_ERROR)
     return HK_STATUS_ERROR;
   if (hk_vm_push_string_from_chars(vm, -1, "sign_hash") == HK_STATUS_ERROR)
     return HK_STATUS_ERROR;
@@ -94,5 +144,5 @@ int32_t load_secp256r1(hk_vm_t *vm)
     return HK_STATUS_ERROR;
   if (hk_vm_push_new_native(vm, "verify_signature", 3, &verify_signature_call) == HK_STATUS_ERROR)
     return HK_STATUS_ERROR;
-  return hk_vm_construct(vm, 3);
+  return hk_vm_construct(vm, 9);
 }
