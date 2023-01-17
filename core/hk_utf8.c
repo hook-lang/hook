c1cd7e0e46980bf26a321b0eaa66c6ce95f78be4
Fabio de Souza Villaca Medeiros <fabiosvm@outlook.com>
2023-01-17 20:10:21 -0300

    added utf8 code module

---
+++ core/hk_utf8.c
@@ -0,0 +1,51 @@
+//
+// The Hook Programming Language
+// hk_utf8.c
+//
+
+#include "hk_utf8.h"
+#include "hk_status.h"
+
+static inline int decode_char(unsigned char c);
+static int32_t len_call(hk_vm_t *vm, hk_value_t *args);
+
+static inline int decode_char(unsigned char c)
+{
+  if ((c & 0xc0) == 0x80)
+    return 0;
+  if ((c & 0xf8) == 0xf0)
+    return 4;
+  if ((c & 0xf0) == 0xe0)
+    return 3;
+  if ((c & 0xe0) == 0xc0)
+    return 2;
+  return 1;
+}
+
+static int32_t len_call(hk_vm_t *vm, hk_value_t *args)
+{
+  if (hk_vm_check_string(args, 1) == HK_STATUS_ERROR)
+    return HK_STATUS_ERROR;
+  hk_string_t *str = hk_as_string(args[1]);
+  int32_t result = 0;
+  for (int32_t i = 0; i < str->length;)
+  {
+    int32_t len = decode_char((unsigned char) str->chars[i]);
+    if (!len)
+      break;
+    i += len;
+    ++result;
+  }
+  return hk_vm_push_float(vm, result);
+}
+
+HK_LOAD_FN(utf8)
+{
+  if (hk_vm_push_string_from_chars(vm, -1, "utf8") == HK_STATUS_ERROR)
+    return HK_STATUS_ERROR;
+  if (hk_vm_push_string_from_chars(vm, -1, "len") == HK_STATUS_ERROR)
+    return HK_STATUS_ERROR;
+  if (hk_vm_push_new_native(vm, "len", 1, &len_call) == HK_STATUS_ERROR)
+    return HK_STATUS_ERROR;
+  return hk_vm_construct(vm, 1);
+}
---
+++ core/hk_utf8.h
@@ -0,0 +1,14 @@
+//
+// The Hook Programming Language
+// hk_utf8.h
+//
+
+#ifndef HK_UTF8_H
+#define HK_UTF8_H
+
+#include "hk_vm.h"
+#include "hk_utils.h"
+
+HK_LOAD_FN(utf8);
+
+#endif // HK_UTF8_H
