acecc7421988538c4c003f07f14a694a4ff5629b
Fabio de Souza Villaca Medeiros <fabiosvm@outlook.com>
2023-01-25 15:11:18 -0300

    added in regex: find()

--- core/hk_regex.c
+++ core/hk_regex.c
@@ -18,6 +18,7 @@ typedef struct
 static inline regex_wrapper_t *regex_wrapper_new(regex_t regex);
 static void regex_wrapper_deinit(hk_userdata_t *udata);
 static int32_t new_call(hk_vm_t *vm, hk_value_t *args);
+static int32_t find_call(hk_vm_t *vm, hk_value_t *args);
 static int32_t is_match_call(hk_vm_t *vm, hk_value_t *args);
 
 static inline regex_wrapper_t *regex_wrapper_new(regex_t regex)
@@ -37,9 +38,9 @@ static int32_t new_call(hk_vm_t *vm, hk_value_t *args)
 {
   if (hk_vm_check_string(args, 1) == HK_STATUS_ERROR)
     return HK_STATUS_ERROR;
-  hk_string_t *str = hk_as_string(args[1]);
+  hk_string_t *pattern = hk_as_string(args[1]);
   regex_t regex;
-  int32_t errcode = regcomp(&regex, str->chars, REG_EXTENDED);
+  int32_t errcode = regcomp(&regex, pattern->chars, REG_EXTENDED);
   if (errcode)
   {
     char errbuf[1024];
@@ -48,6 +49,35 @@ static int32_t new_call(hk_vm_t *vm, hk_value_t *args)
     return HK_STATUS_ERROR;
   }
   return hk_vm_push_userdata(vm, (hk_userdata_t *) regex_wrapper_new(regex));
+}
+
+static int32_t find_call(hk_vm_t *vm, hk_value_t *args)
+{
+  if (hk_vm_check_userdata(args, 1) == HK_STATUS_ERROR)
+    return HK_STATUS_ERROR;
+  if (hk_vm_check_string(args, 2) == HK_STATUS_ERROR)
+    return HK_STATUS_ERROR;
+  regex_wrapper_t *wrapper = (regex_wrapper_t *) hk_as_userdata(args[1]);
+  hk_string_t *str = hk_as_string(args[2]);
+  regmatch_t match;
+  int32_t errcode = regexec(&wrapper->regex, str->chars, 1, &match, 0);
+  if (errcode)
+  {
+    if (errcode == REG_NOMATCH)
+      return hk_vm_push_nil(vm);
+    char errbuf[1024];
+    regerror(errcode, &wrapper->regex, errbuf, sizeof(errbuf));
+    hk_runtime_error("cannot match regex: %s", errbuf);
+    return HK_STATUS_ERROR;
+  }
+  int32_t start = match.rm_so;
+  int32_t end = match.rm_eo;
+  if (start == -1 || end == -1)
+    return hk_vm_push_nil(vm);
+  hk_array_t *result = hk_array_new_with_capacity(2);
+  hk_array_inplace_add_element(result, hk_float_value(start));
+  hk_array_inplace_add_element(result, hk_float_value(end));
+  return hk_vm_push_array(vm, result);
 }
 
 static int32_t is_match_call(hk_vm_t *vm, hk_value_t *args)
@@ -79,9 +109,13 @@ HK_LOAD_FN(regex)
     return HK_STATUS_ERROR;
   if (hk_vm_push_new_native(vm, "new", 1, &new_call) == HK_STATUS_ERROR)
     return HK_STATUS_ERROR;
+  if (hk_vm_push_string_from_chars(vm, -1, "find") == HK_STATUS_ERROR)
+    return HK_STATUS_ERROR;
+  if (hk_vm_push_new_native(vm, "find", 2, &find_call) == HK_STATUS_ERROR)
+    return HK_STATUS_ERROR;
   if (hk_vm_push_string_from_chars(vm, -1, "is_match") == HK_STATUS_ERROR)
     return HK_STATUS_ERROR;
-  if (hk_vm_push_new_native(vm, "is_match", 1, &is_match_call) == HK_STATUS_ERROR)
+  if (hk_vm_push_new_native(vm, "is_match", 2, &is_match_call) == HK_STATUS_ERROR)
     return HK_STATUS_ERROR;
-  return hk_vm_construct(vm, 2);
+  return hk_vm_construct(vm, 3);
 }
