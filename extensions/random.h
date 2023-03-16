11eee9ea7540fb30df489c4a0b32d5f4fbbf68d2
Fabio de Souza Villaca Medeiros <fabiosvm@outlook.com>
2023-03-16 16:07:34 -0300

    added random extension

---
+++ extensions/random.h
@@ -0,0 +1,14 @@
+//
+// The Hook Programming Language
+// random.h
+//
+
+#ifndef RANDOM_H
+#define RANDOM_H
+
+#include <hook/state.h>
+#include <hook/utils.h>
+
+HK_LOAD_FN(random);
+
+#endif // RANDOM_H
